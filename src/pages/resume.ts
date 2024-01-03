dd71070b15d3351b08e4c19bab146ff859d083da
Cameron DeCoster <cam@observablehq.com>
2024-01-03 16:20:49 -0700

    fix: Switches resume endpoint to fetch from Drive

--- src/pages/resume.ts
+++ src/pages/resume.ts
@@ -2,4 +2,7 @@ import type { APIRoute } from "astro";
 
 const path = "https://docs.google.com/document/d/1juuz3vISqaGdMOt0wzDf_jfugJrqt_CqnqIqHvAJBrU/export?format=pdf"
 
-export const GET: APIRoute = ({ redirect }) => redirect(path, 304);
+export const GET: APIRoute = async () => {
+  const response = await fetch(path);
+  return new Response(await response.arrayBuffer());
+}
