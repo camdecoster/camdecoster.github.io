651f50d54512a120959bc63a2f59b92137a17fd6
Cameron DeCoster <cameron.decoster@gmail.com>
2024-09-04 14:07:50 -0600

    Adds dynamic meta image for blog posts (#46)
    * Moves/deletes images from public folder
    
    * Switches About page to use images from assets
    
    * Switches to specific meta image for head element

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,14 +3,14 @@ import { AUTHOR } from "../consts";
 
 const blog = defineCollection({
   // Type-check frontmatter using a schema
-  schema: z.object({
+  schema: ({ image }) => z.object({
     author: z.string().default(AUTHOR).optional(),
     title: z.string(),
     description: z.string(),
     // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-    image: z.string().optional(),
+    image: image().optional(),
     tags: z.array(z.string()).default([]).optional(),
     draft: z.boolean().optional(),
     featured: z.boolean().optional(),
