b3786bde1fb18b6e87146564aae91d608845465c
Cameron DeCoster <cam@observablehq.com>
2024-12-11 18:56:16 -0700

    feat: Adds inline Shiki syntax highlighting

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,11 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import rehypeShiki from "@shikijs/rehype"
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro"
+import ahkGrammar from "./src/grammars/ahk2.tmLanguage.json";
+
+const SHIKI_THEME = "one-dark-pro";
 
 export default defineConfig({
   site: "https://camdecoster.dev",
@@ -11,8 +15,21 @@ export default defineConfig({
     UnoCSS({ injectReset: true }),
   ],
   markdown: {
+    rehypePlugins: [
+      [
+        rehypeShiki,
+        {
+          inline: "tailing-curly-colon",
+          theme: SHIKI_THEME,
+        },
+      ],
+    ],
     shikiConfig: {
-      theme: "one-dark-pro",
+      langs: [{
+        ...ahkGrammar,
+        aliases: ["ahk"],
+      }],
+      theme: SHIKI_THEME,
     },
   },
 });
